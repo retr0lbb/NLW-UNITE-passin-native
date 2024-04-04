d0f2504582458aa71447f57f60d162aab8b0bb4e
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-04-04 00:59:27 -0300

    feat: add components and loading screen for fonts to load

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,25 @@
-import "../styles/global.css";
-import {Slot} from "expo-router";
+import "@/styles/global.css";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar"
+import {Loading} from "@/components/loading"
+import { 
+    Roboto_700Bold, 
+    Roboto_400Regular, 
+    Roboto_500Medium, 
+    useFonts 
+} from "@expo-google-fonts/roboto"
 
 export default function Layout(){
+    const [ fontLoaded ] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold
+    })
+
     return(
-        <Slot />
+        <>
+            <StatusBar style="light" />
+            {fontLoaded? <Slot /> : <Loading />}
+        </>
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,25 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Image } from "react-native"
+import { Input } from "@/components/input"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors"
 
 export default function Home(){
     return(
-        <View className="">
-            <Text >Hello World!</Text>
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <Image 
+            source={require("@/assets/logo.png")} 
+            className="h-16" 
+            resizeMode="contain"/>
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                <MaterialCommunityIcons 
+                name="ticket-confirmation-outline" 
+                size={20} 
+                color={colors.green[200]}/>
+                    <Input.Field placeholder="Insira o seu codigo de ingresso"/>
+                    
+                </Input>
+            </View>
         </View>
     )
 }
