8ce9a5a3601cdf94928966468e0821b43ef7be4d
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-04-04 00:12:29 -0300

    feat: add tailwind css to mobile

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function Home(){
+    return(
+        <View className="">
+            <Text >Hello World!</Text>
+        </View>
+    )
+}
+
+
